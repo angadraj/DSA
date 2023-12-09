015ee90ec9d583ca072c26524917d62f1cf88408
Angad Raj Singh <angad.singh@fuelbuddy.in>
2023-12-09 12:24:07 +0530

    wip

--- recursion/Level_1.java
+++ recursion/Level_1.java
@@ -14,14 +14,83 @@ public class Level_1 {
     }
 
     public static void printIncreasing (int n) {
-        if (n == 10) {
+        if (n == 0) {
             return;
         }
+
+        printIncreasing(n - 1);
         System.out.print(n + " ");
-        printIncreasing(n + 1);
+    }
+
+    public static void printDI (int n) {
+        if (n == 0) return;
+        System.out.print(n + " ");
+        printDI(n - 1);
+        System.out.print(n + " ");
+    }
+
+    public static int factorial (int n) {
+        if (n == 1) return 1;
+        n *= factorial(n - 1);
+        return n;
+    }
+
+    // O(n)
+    public static int power (int n, int p) {
+        if (p == 1) return n;
+        n *= power(n, p - 1);
+        return n;
+    }
+
+    // O(logn)
+    public static int powerLog(int n, int p) {
+        if (p == 1) return n;
+
+        int halfPow = powerLog(n, p / 2);
+        int val = (halfPow * halfPow);
+        if (p % 2 == 1) {
+            // if power is odd
+            val *= n;
+        }
+        return val;
+    }
+
+    // print zig zag
+    // fn = n + f(n - 1) + n + f(n - 1) + n
+    // f(2) = 2 + f(1) + 2 + f(1) + 2
+    // f(1) = 1 + 0 + 1 + 0 + 1
+
+    // f(2) = 2 + (1 + 0 + 1 + 0 + 1) + 2 + (1 + 0 + 1 + 0 + 1) + 2
+    // f(3) = 3 + (2 + (1 + 0 + 1 + 0 + 1) + 2 + (1 + 0 + 1 + 0 + 1) + 2) + 3 + (2 + (1 + 0 + 1 + 0 + 1) + 2 + (1 + 0 + 1 + 0 + 1) + 2) + 3
+    public static void pzz(int n) {
+        if (n == 0) return;
+        System.out.print(n + " ");
+        pzz(n - 1);
+        System.out.print(n + " ");
+        pzz(n - 1);
+        System.out.print(n + " ");
+    }
+
+    // tower of hanoi
+    /**
+     * f(1, a, b, c) = a -> b (1, a, b, c)
+     * f(2, a, b, c) = a -> c, a -> b, c -> b (2, a, c, b)
+     * f(2, a, b, c) = f(1, a, c, b) + (a -> b) + f(1, c, b, a)
+     * f(3, a, b, c) = f(2, a, c, b) + (a -> b) + f(2, c, b, a)
+     */
+    public static void toh(int n, String a, String b, String c) {
+        if (n == 1) {
+            System.out.println(a + " -> " + b);
+            return;
+        }
+        toh(n - 1, a, c, b);
+        System.out.println(a + " -> " + b);
+        toh(n - 1, c, b, a);
     }
 
     public static void main (String[] args) {
-        printIncreasing(0);
+        // int ans = powerLog(2, 10);
+        // System.out.print(ans);
+        toh(3, "a", "b", "c");
     }
 }
